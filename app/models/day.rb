35824c5347369046d3fe308c8f3a6aac27b4a888
george <george172@outlook.com>
2025-11-11 19:36:17 +0100

    Add callback to handle not null validations caused by previous commit

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,4 +1,13 @@
 class Day < ApplicationRecord
   has_many :meals, dependent: :destroy
   belongs_to :user
+
+  before_validation :set_nil_goals_to_zero
+
+  private
+
+  def set_nil_goals_to_zero
+    self.calorie_goal = 0 if calorie_goal.nil?
+    self.protein_goal = 0 if protein_goal.nil?
+  end
 end
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,3 +1,12 @@
 class Meal < ApplicationRecord
   belongs_to :day
+
+  before_validation :set_nil_nutrition_to_zero
+
+  private
+
+  def set_nil_nutrition_to_zero
+    self.calories = 0 if calories.nil?
+    self.protein  = 0 if protein.nil?
+  end
 end
